dfd7fe506ad9823f11f4d5f67285cc653401610e
rammah karpous <rammahkarpous@outlook.com>
2024-04-09 15:18:04 +0100

    install tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.liquid"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./**/*.liquid"],
+	theme: {
+		extend: {
+			colors: {
+				body: "#F8F8F8",
+				white: "#FFFFFF",
+				black: "#05102B",
+				orange: "#C98C1C",
+				yellow: "#EBBF23",
+				blue: {
+					dark: "#263761",
+					medium: "#405CA1",
+					light: "#405DA1"
+				},
+				gray: {
+					medium: "#00000029",
+					border: "#E9EAEE",
+				}
+			}
+		},
+	},
+	plugins: [],
 }
 
